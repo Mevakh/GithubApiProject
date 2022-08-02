007f1f1be955e8f6c53ccc56c0879bebf40d8f1d
Mevakh <107620982+Mevakh@users.noreply.github.com>
2022-08-02 19:17:40 +0300

    Github api projesi
    Kurstan örnek alınarak yapılmıştır

---
+++ storage.js
@@ -0,0 +1,26 @@
+class Storage{
+    static getSearchedusersFromStorage(){
+        let users;
+        if(localStorage.getItem("searched") === null){
+            users = [];
+        }
+        else {
+            users = JSON.parse(localStorage.getItem("searched"));
+        }
+        return users;
+
+    }
+    static addSearchedUserToStorage(username){
+        let users = this.getSearchedusersFromStorage();
+
+        if (users.indexOf(username) === -1){
+            users.push(username);
+        }
+        localStorage.setItem("searched",JSON.stringify(users));
+
+    }
+    static clearAllSearchedUsersFromStorage(){
+        localStorage.removeItem("searched");
+
+    }
+}
